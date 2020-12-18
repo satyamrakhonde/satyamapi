44a4a12bdf79adc8ca3b325c6a72a80fa80497cd
satyamrakhonde <satyamrakhonde@gamil.com>
2020-12-18 18:00:04 +0530

    commit

---
+++ routes/users.js
@@ -0,0 +1,22 @@
+import express from 'express';
+
+
+import { createUser, getUsers, getUser, deleteUser, updateUser} from '../controllers/users.js';
+
+const router = express.Router();
+
+
+
+//all routes in here are starting with /users
+router.get('/', getUsers);
+
+router.post('/', createUser);
+
+// /users/2 => req.params { id: 2 }
+router.get('/:id', getUser);
+
+router.delete('/:id', deleteUser);
+
+router.patch('/:id', updateUser);
+
+export default router;
